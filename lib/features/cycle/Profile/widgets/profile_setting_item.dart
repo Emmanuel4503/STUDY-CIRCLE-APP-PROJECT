a313f7640d2a9af4196a28ea62e643387138d65f
Techtopsy <topmanze@gmail.com>
2026-08-20 13:52:19 +0100

    fix deprecated

--- lib/features/cycle/Profile/widgets/profile_setting_item.dart
+++ lib/features/cycle/Profile/widgets/profile_setting_item.dart
@@ -31,7 +31,7 @@ class ProfileSettingItem extends StatelessWidget {
           ),
           boxShadow: [
             BoxShadow(
-              color: Colors.grey.withOpacity(0.05),
+              color: Colors.grey.withValues(alpha: 0.05),
               blurRadius: 10,
               offset: const Offset(0, 4),
             ),
@@ -42,7 +42,10 @@ class ProfileSettingItem extends StatelessWidget {
             Container(
               padding: const EdgeInsets.all(SSizes.sm),
               decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
+                color: Theme.of(context)
+                    .colorScheme
+                    .primary
+                    .withValues(alpha: 0.1),
                 shape: BoxShape.circle,
               ),
               child: Icon(
